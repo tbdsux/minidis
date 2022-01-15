0c3464b326ae262a27282e762ee0cd19d36f8add
TheBoringDude <iamcoderx@gmail.com>
2022-01-15 11:23:57 +0800

    Update simple example

--- example/simple/main.go
+++ example/simple/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/World-of-Cryptopups/minidis"
 	"github.com/bwmarrin/discordgo"
@@ -15,6 +16,9 @@ func main() {
 	// set intents
 	bot.SetIntents(discordgo.IntentsGuilds | discordgo.IntentsGuildMessages)
 
+	//
+	bot.SyncToGuilds(os.Getenv("GUILD"))
+
 	bot.OnReady(func(s *discordgo.Session, i *discordgo.Ready) {
 		log.Println("Bot is ready!")
 	})
@@ -25,15 +29,28 @@ func main() {
 		Description: "Simple ping command.",
 		Options:     []*discordgo.ApplicationCommandOption{},
 		Execute: func(c *minidis.SlashContext) error {
-			_, err := c.ReplyString(fmt.Sprintf("Hello **%s**, pong?", c.Author.Username))
+			return c.ReplyString(fmt.Sprintf("Hello **%s**, pong?", c.Author.Username))
 
-			return err
 		},
 	})
 
-	// if err := bot.SyncCommands("751230186594500689"); err != nil {
-	// 	log.Fatalf("Failed to sync commands: %v\n", err)
-	// }
+	bot.AddCommand(minidis.SlashCommandProps{
+		Command:     "response",
+		Description: "Responses management.",
+		Options:     []*discordgo.ApplicationCommandOption{},
+		Execute: func(s *minidis.SlashContext) error {
+			if err := s.ReplyString("Hello! this message will be modified after 5 seconds..."); err != nil {
+				_, err = s.Followup("A problem has occured")
+				return err
+			}
 
-	bot.Run()
+			time.Sleep(time.Second * 5)
+
+			return s.Edit("new message here!")
+		},
+	})
+
+	if err := bot.Run(); err != nil {
+		log.Fatalln(err)
+	}
 }
